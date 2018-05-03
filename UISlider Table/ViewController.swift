cef231f810935e51cab3a4ace7e429bfba93e1c2
Jonghyun Kim <jhkim@dit.ac.kr>
2018-05-03 11:27:29 +0900

    TableView, UISlider

--- UISlider Table/ViewController.swift
+++ UISlider Table/ViewController.swift
@@ -8,18 +8,42 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
+
+    @IBOutlet weak var mySlider: UISlider!
+    
+    @IBOutlet weak var myTableView: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        //mySlider.setValue(0.0, animated: true)
+        myTableView.dataSource = self
+        myTableView.delegate = self
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func sliderMoved(_ sender: Any) {
+        
+        print("slider vlaue = \(Int(mySlider.value * 100))")
+        
+        // tableView의 값을 reload
+        myTableView.reloadData()
     }
-
-
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 100
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let identifier = "RE"
+        let cell = myTableView.dequeueReusableCell(withIdentifier: identifier, for: indexPath)
+        
+        
+//        cell.textLabel?.text = String(Int(mySlider.value))
+        let tableValue = String(Int(mySlider.value * 100) + indexPath.row)
+        cell.textLabel?.text = tableValue
+        return  cell
+    }
+    
 }
 
