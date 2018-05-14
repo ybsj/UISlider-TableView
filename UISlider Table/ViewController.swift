a392b96a8baae27fde3f813f7ea1f628e2f57d0d
D7703_07 <d7703_07@d7703-07ui-imac.local>
2018-05-14 15:53:32 +0900

    123123

--- UISlider Table/ViewController.swift
+++ UISlider Table/ViewController.swift
@@ -8,39 +8,32 @@
 
 import UIKit
 
-class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
-
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     @IBOutlet weak var mySlider: UISlider!
-    
+    @IBOutlet weak var myLabel: UILabel!
     @IBOutlet weak var myTableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-        //mySlider.setValue(0.0, animated: true)
         myTableView.dataSource = self
         myTableView.delegate = self
     }
-
+    
+    
     @IBAction func sliderMoved(_ sender: Any) {
-        
-        print("slider vlaue = \(Int(mySlider.value * 100))")
-        
-        // tableView의 값을 reload
         myTableView.reloadData()
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 100
+        return 101
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let identifier = "RE"
-        let cell = myTableView.dequeueReusableCell(withIdentifier: identifier, for: indexPath)
+        let cell = myTableView.dequeueReusableCell(withIdentifier: "RE", for: indexPath)
         
-        let tableValue = String(Int(mySlider.value * 100) + indexPath.row)
-        cell.textLabel?.text = tableValue
-        return  cell
+        _ = String(Int(mySlider.value) + indexPath.row)
+        cell.textLabel?.text = String(Int(mySlider.value) + indexPath.row)
+        return cell
     }
     
 }
-
